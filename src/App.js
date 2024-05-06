246a56a0439ced0dc8057172a02b86eeff8b0102
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-06 21:57:56 +0530

    updated: navigation bar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className='navigation-items'>
-     
+
+    <div>
+      <div className='navigation-items'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfJx10T5pdqHuUjlB8o910koS1G9X1_UGZsQ&s' alt='logo' />
+        <p>PragyaAni  Gallery</p>
+
+
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Contact Us</li>
+          <li>About Us</li>
+        </ul>
+
+
+      </div>
+
     </div>
   );
 }

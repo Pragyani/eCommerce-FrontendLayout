cd22dd7999d4ce1db359c7b517df8e9adf0963f8
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-06 20:09:15 +0530

    Adding:navigation bar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <div className='logo'>
+          <img src='/images/PA-AP-logo-design-vector-Graphics-20829779-1-580x386.jpg' alt='logo' />
+        </div>
+
+        <ul>
+          <li>Menu</li>
+          <li>Home</li>
+          <li>contact</li>
+          <li>About Us</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;

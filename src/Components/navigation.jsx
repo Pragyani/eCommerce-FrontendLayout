6519d5ef6fbdb950af6b7cebeac10d6612ddb367
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-07 22:36:18 +0530

    Optimization in Component

--- src/Components/navigation.jsx
+++ src/Components/navigation.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import ImgLogo from "../images/logoImg";
 
 const NavigationBar =()=>{
     return(
         <>
          <div className='navigation-items'>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfJx10T5pdqHuUjlB8o910koS1G9X1_UGZsQ&s' alt='logo' />
+        <img src={ImgLogo} alt='logo' />
         <p>PragyAni  Gallery</p>
-
-
         <ul>
           <li>Home</li>
           <li>Menu</li>

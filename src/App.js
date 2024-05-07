f18aae0b469f54cfb2de1dc0535773b2e7179730
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-07 16:26:37 +0530

    Adding :heading on the page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div>
       <div className='navigation-items'>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfJx10T5pdqHuUjlB8o910koS1G9X1_UGZsQ&s' alt='logo' />
-        <p>PragyaAni  Gallery</p>
+        <p>PragyAni  Gallery</p>
 
 
         <ul>
@@ -15,8 +15,10 @@ function App() {
           <li>Contact Us</li>
           <li>About Us</li>
         </ul>
+      </div>
 
-
+      <div className='top-line'>
+        <h2>"Enhancing Homes with Classic Charm"</h2>
       </div>
 
     </div>

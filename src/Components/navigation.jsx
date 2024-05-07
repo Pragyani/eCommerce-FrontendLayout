24a76500f6c4d95a3b4b14094cb439766f96fb6a
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-07 16:31:21 +0530

    Adding : NavigationBar componenet

---
+++ src/Components/navigation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NavigationBar =()=>{
+    return(
+        <>
+         <div className='navigation-items'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfJx10T5pdqHuUjlB8o910koS1G9X1_UGZsQ&s' alt='logo' />
+        <p>PragyAni  Gallery</p>
+
+
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Contact Us</li>
+          <li>About Us</li>
+        </ul>
+      </div>
+        </>
+    )
+}
+export  default NavigationBar;

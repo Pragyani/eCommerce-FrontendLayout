7bd7b2c47fab960a0c583fc1745e93c5608f6058
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-07 16:30:44 +0530

    Optimiztion in Componenet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
+import React from 'react';
+import NavigationBar from './Components/navigation';
 import './App.css';
 
 function App() {
   return (
 
     <div>
-      <div className='navigation-items'>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfJx10T5pdqHuUjlB8o910koS1G9X1_UGZsQ&s' alt='logo' />
-        <p>PragyAni  Gallery</p>
-
-
-        <ul>
-          <li>Home</li>
-          <li>Menu</li>
-          <li>Contact Us</li>
-          <li>About Us</li>
-        </ul>
-      </div>
-
+      <NavigationBar />
+      
       <div className='top-line'>
         <h2>"Enhancing Homes with Classic Charm"</h2>
       </div>

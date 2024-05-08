c9acb44a5964fa729e4eabf212bbd8a0590d8492
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-08 11:52:30 +0530

    Adding: Container Component

---
+++ src/Components/mainContainer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Container =()=>{
+    return(
+        <>
+        <div className='main-container'>
+             <div className='conatiner'>
+          <img src='https://plus.unsplash.com/premium_photo-1673514503009-912ffc6ff956?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='conatainer-img' />
+
+          <div className='para'>
+            <p>Express Yourself </p>
+            <p>With </p>
+            <p>Expectional Art </p>
+            <p>Pieces</p>
+          </div>
+              </div>
+      </div>
+        </>
+    )
+}
+export default Container

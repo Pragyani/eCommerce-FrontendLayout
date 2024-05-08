e2a4bb96a3170d413473177160a968cc312fa3ce
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-08 11:52:55 +0530

    Added: top heading component

---
+++ src/Components/TopHeading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeadigLine = () => {
+    return (
+        <>
+            <div className='top-line'>
+                <h2>"Enhancing Homes with Classic Charm"</h2>
+            </div>
+
+        </>
+    )
+}
+export default HeadigLine;

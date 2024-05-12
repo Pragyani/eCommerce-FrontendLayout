b0ba707d5c1fa7e577f908f2a2c7648ddb916ac9
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-12 19:47:23 +0530

    Adding new files in floder

---
+++ src/Components/header/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeadigLine = () => {
+    return (
+        <>
+            <div className='top-line'>
+                <h2>"Enhancing Homes with Classic Charm"</h2>
+            </div>
+            <hr></hr>
+
+        </>
+    )
+}
+export default HeadigLine;

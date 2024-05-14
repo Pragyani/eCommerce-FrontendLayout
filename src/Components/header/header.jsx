3a6b3eb50d75bac1b9434aa2da3b6e32625d2958
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-14 13:59:55 +0530

    Adding: modifed componenet

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,14 +1,33 @@
 import React from "react";
+import "../header/header.css"
 
-const HeadigLine = () => {
+const Header = () => {
     return (
         <>
-            <div className='top-line'>
-                <h2>"Enhancing Homes with Classic Charm"</h2>
-            </div>
-            <hr></hr>
+            <header>
+                <div className="container-header">
+                    <div className="row">
+                        <div className="columns">
+                            <img src="https://raw.githubusercontent.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/8ec2e9f2872233b520ce62eaef27d4e96268d0de/src/assets/images/logo.svg" alt="logo" />
+                        </div>
+
+                        <div className="column-sm-5">
+                            <div className="headerSearch d-flex align-items-center">
+                                 <div className="selecrDrop cursor">
+                                    All Categories
+                                 </div>
+
+                                 <div className="search">
+                                    <input type="text" placeholder="Search"/>
+                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
 
         </>
     )
 }
-export default HeadigLine;
+
+export default Header

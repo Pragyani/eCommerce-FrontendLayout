78c99eb5f89030f9cd875ea37ddcdbdd3a95ffa1
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-15 12:44:43 +0530

    Adding: list in the dropper categories field

--- src/Components/selectDrop/SelectDroper.js
+++ src/Components/selectDrop/SelectDroper.js
@@ -6,7 +6,27 @@ const Selection =()=>{
     return(
         <>
         <div className="selectDrop">
+           <div className="searchFeild">
+                 <input type="text"/>
+           </div>
 
+           <ul>
+            <li>Milks & Daires</li>
+            <li>Wine & Drink</li>
+            <li>Bread and Juices</li>
+            <li>Clothing & Beauty</li>
+            <li>Fresh Seafood</li>
+            <li>Pet Foor & Toy</li>
+            <li>Bakery Product</li>
+            <li>Fresh Fruit </li>
+            <li>Vegetables</li>
+            <li>Fast Food</li>
+            <li>Bakery Product</li>
+            <li>Fresh Fruit </li>
+            <li>Vegetables</li>
+            <li>Fast Food</li>
+
+           </ul>
         </div>
         
         </>

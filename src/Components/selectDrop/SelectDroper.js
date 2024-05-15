06e5ff0fcd1fd1785b07b3593ee677858a0efe94
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-15 15:38:28 +0530

    adding: classname in component

--- src/Components/selectDrop/SelectDroper.js
+++ src/Components/selectDrop/SelectDroper.js
@@ -1,31 +1,37 @@
 import React from "react";
 import "./Selectdroper.css";
-
+import "../header/header.css";
 
 const Selection = () => {
     return (
         <>
-            <div className="selectDrop">
-                <div className="searchFeild">
-                    <input type="text" />
+            <div className="selecrDropWrapper cursor position-relative">
+                <span className="onSelect"> All Categories</span>
+
+                <div className="selectDrop">
+                    <div className="searchFeild">
+                        <input type="text" />
+                    </div>
+                    <ul className="searchResults">
+                        <li>Milks & Daires</li>
+                        <li>Wine & Drink</li>
+                        <li>Bread and Juices</li>
+                        <li>Clothing & Beauty</li>
+                        <li>Fresh Seafood</li>
+                        <li>Pet Foor & Toy</li>
+                        <li>Bakery Product</li>
+                        <li>Fresh Fruit </li>
+                        <li>Vegetables</li>
+                        <li>Fast Food</li>
+                        <li>Bakery Product</li>
+                        <li>Fresh Fruit </li>
+                        <li>Vegetables</li>
+                        <li>Fast Food</li>
+                    </ul>
                 </div>
-                <ul className="searchResults">
-                    <li>Milks & Daires</li>
-                    <li>Wine & Drink</li>
-                    <li>Bread and Juices</li>
-                    <li>Clothing & Beauty</li>
-                    <li>Fresh Seafood</li>
-                    <li>Pet Foor & Toy</li>
-                    <li>Bakery Product</li>
-                    <li>Fresh Fruit </li>
-                    <li>Vegetables</li>
-                    <li>Fast Food</li>
-                    <li>Bakery Product</li>
-                    <li>Fresh Fruit </li>
-                    <li>Vegetables</li>
-                    <li>Fast Food</li>
-                </ul>
             </div>
+
+
 
         </>
     )

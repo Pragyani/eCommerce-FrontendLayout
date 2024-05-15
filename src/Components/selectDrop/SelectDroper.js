c77884d0217aef50616c6132841bae15b1d15b1b
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-15 11:06:06 +0530

    Adding : Classname and importing the css

--- src/Components/selectDrop/SelectDroper.js
+++ src/Components/selectDrop/SelectDroper.js
@@ -1,10 +1,13 @@
 import React  from "react";
-
+import "./Selectdroper.css";
 
 
 const Selection =()=>{
     return(
         <>
+        <div className="selectDrop">
+
+        </div>
         
         </>
     )

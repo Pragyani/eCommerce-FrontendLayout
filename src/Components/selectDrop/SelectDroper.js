b02a99ee157ff1936798e93697cf09002f6ed0de
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-15 17:35:22 +0530

    Added: useState and Condition for components list rendering

--- src/Components/selectDrop/SelectDroper.js
+++ src/Components/selectDrop/SelectDroper.js
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Selectdroper.css";
 import "../header/header.css";
 
 const Selection = () => {
+
+    const [isOpenSelect, setIsOpenSelect] = useState(false);
+
+    const openSelect =()=>{
+        setIsOpenSelect(!isOpenSelect)
+    }
+
     return (
         <>
             <div className="selecrDropWrapper cursor position-relative">
-                <span className="onSelect"> All Categories</span>
+                <span className="onSelect" onClick={openSelect}> All Categories</span>
 
-                <div className="selectDrop">
-                    <div className="searchFeild">
-                        <input type="text" />
-                    </div>
-                    <ul className="searchResults">
-                        <li>Milks & Daires</li>
-                        <li>Wine & Drink</li>
-                        <li>Bread and Juices</li>
-                        <li>Clothing & Beauty</li>
-                        <li>Fresh Seafood</li>
-                        <li>Pet Foor & Toy</li>
-                        <li>Bakery Product</li>
-                        <li>Fresh Fruit </li>
-                        <li>Vegetables</li>
-                        <li>Fast Food</li>
-                        <li>Bakery Product</li>
-                        <li>Fresh Fruit </li>
-                        <li>Vegetables</li>
-                        <li>Fast Food</li>
-                    </ul>
-                </div>
+                {isOpenSelect === true &&
+                    <div className="selectDrop">
+                        <div className="searchFeild">
+                            <input type="text" />
+                        </div>
+                        <ul className="searchResults">
+                            <li>Milks & Daires</li>
+                            <li>Wine & Drink</li>
+                            <li>Bread and Juices</li>
+                            <li>Clothing & Beauty</li>
+                            <li>Fresh Seafood</li>
+                            <li>Pet Foor & Toy</li>
+                            <li>Bakery Product</li>
+                            <li>Fresh Fruit </li>
+                            <li>Vegetables</li>
+                            <li>Fast Food</li>
+                            <li>Bakery Product</li>
+                            <li>Fresh Fruit </li>
+                            <li>Vegetables</li>
+                            <li>Fast Food</li>
+                        </ul>
+                    </div>}
             </div>
 
 

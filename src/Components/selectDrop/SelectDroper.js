9ccae05f60785725e3b2e7f60a7003b3b2155eb5
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-16 17:44:27 +0530

    added: onClick ,classname in list ,States

--- src/Components/selectDrop/SelectDroper.js
+++ src/Components/selectDrop/SelectDroper.js
@@ -5,9 +5,16 @@ import "../header/header.css";
 const Selection = () => {
 
     const [isOpenSelect, setIsOpenSelect] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0)
 
-    const openSelect =()=>{
+    const openSelect = () => {
         setIsOpenSelect(!isOpenSelect)
+    }
+
+
+    const closeSelect = (index) => {
+        setSelectedIndex(index)
+        setIsOpenSelect(false)
     }
 
     return (
@@ -21,20 +28,20 @@ const Selection = () => {
                             <input type="text" />
                         </div>
                         <ul className="searchResults">
-                            <li>Milks & Daires</li>
-                            <li>Wine & Drink</li>
-                            <li>Bread and Juices</li>
-                            <li>Clothing & Beauty</li>
-                            <li>Fresh Seafood</li>
-                            <li>Pet Foor & Toy</li>
-                            <li>Bakery Product</li>
-                            <li>Fresh Fruit </li>
-                            <li>Vegetables</li>
-                            <li>Fast Food</li>
-                            <li>Bakery Product</li>
-                            <li>Fresh Fruit </li>
-                            <li>Vegetables</li>
-                            <li>Fast Food</li>
+                            <li onClick={() => closeSelect(0)} className={`${selectedIndex===0 ? 'active' : ''}`}>All Categories</li>
+                            <li onClick={() => closeSelect(1)} className={`${selectedIndex===1 ? 'active' : ''}`}>Milks & Daires</li>
+                            <li onClick={() => closeSelect(2)} className={`${selectedIndex===2 ? 'active' : ''}`}>Wine & Drink</li>
+                            <li onClick={() => closeSelect(3)} className={`${selectedIndex===3 ? 'active' : ''}`}>Clothing & Beauty</li>
+                            <li onClick={() => closeSelect(4)} className={`${selectedIndex===4 ? 'active' : ''}`}>Fresh Seafood</li>
+                            <li onClick={() => closeSelect(5)} className={`${selectedIndex===5 ? 'active' : ''}`}>Pet Foor & Toy</li>
+                            <li onClick={() => closeSelect(6)} className={`${selectedIndex===6 ? 'active' : ''}`}>Bakery Product</li>
+                            <li onClick={() => closeSelect(7)} className={`${selectedIndex===7 ? 'active' : ''}`}>Fresh Fruit </li>
+                            <li onClick={() => closeSelect(8)} className={`${selectedIndex===8 ? 'active' : ''}`}>Vegetables</li>
+                            <li onClick={() => closeSelect(9)} className={`${selectedIndex===9 ? 'active' : ''}`}>Fast Food</li>
+                            <li onClick={() => closeSelect(10)} className={`${selectedIndex===10 ? 'active' : ''}`}>Bakery Product</li>
+                            <li onClick={() => closeSelect(11)} className={`${selectedIndex===11 ? 'active' : ''}`}>Fresh Fruit </li>
+                            <li onClick={() =>closeSelect(12)} className={`${selectedIndex===12 ? 'active' : ''}`}>Vegetables</li>
+                            <li onClick={() => closeSelect(13)} className={`${selectedIndex===13? 'active' : ''}`}>Fast Food</li>
                         </ul>
                     </div>}
             </div>

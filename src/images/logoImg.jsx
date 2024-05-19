8df3ff9df9f3261d636fd220b94349ab3cff36f7
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-20 00:49:45 +0530

    clean the code

--- src/images/logoImg.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const ImgLogo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfJx10T5pdqHuUjlB8o910koS1G9X1_UGZsQ&s'
-export default ImgLogo;
-
-export const containerIMG = 'https://plus.unsplash.com/premium_photo-1673514503009-912ffc6ff956?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'

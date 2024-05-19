2ceee469b1c2885c7df7f6f9e5f8d0a20987b838
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-20 00:40:29 +0530

    Adding: Images Component

---
+++ src/Components/imagesURL/Images.jsx
@@ -0,0 +1,13 @@
+
+
+const logoImg = "https://raw.githubusercontent.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/8ec2e9f2872233b520ce62eaef27d4e96268d0de/src/assets/images/logo.svg"
+
+export default logoImg;
+
+ export const compreIcon ="https://raw.githubusercontent.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/8ec2e9f2872233b520ce62eaef27d4e96268d0de/src/assets/images/icon-compare.svg"
+
+ export const WhisListIcon= "https://raw.githubusercontent.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/8ec2e9f2872233b520ce62eaef27d4e96268d0de/src/assets/images/icon-heart.svg"
+
+ export const cartIcon= "https://raw.githubusercontent.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/8ec2e9f2872233b520ce62eaef27d4e96268d0de/src/assets/images/icon-cart.svg"
+
+ export const userIcon = "https://raw.githubusercontent.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/8ec2e9f2872233b520ce62eaef27d4e96268d0de/src/assets/images/icon-user.svg"

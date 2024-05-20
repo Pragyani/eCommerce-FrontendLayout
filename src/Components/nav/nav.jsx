382a63bb0b32caff9d09c953f718e1c8cad665af
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-20 13:53:53 +0530

    adding: nav jsx componet

---
+++ src/Components/nav/nav.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+ 
+const Nav =()=>{
+    return(
+        <>
+        <div className="nav">
+            <div className="container d-flex">
+                <div className="row-line">
+                    <div className="browser-block">
+
+                    </div>
+
+                    <div className="nav-content">
+
+                    </div>
+                    
+                    <div className="contact-block">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        </>
+    )
+}
+
+export default Nav;

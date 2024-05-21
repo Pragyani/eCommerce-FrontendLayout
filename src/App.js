14b51b40fa4a8441aa81b2bad2da244a5b9ed32f
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-21 18:27:01 +0530

    adding: routes from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/header/header';
+import { BrowserRouter , Routes , Route} from 'react-router-dom';
+import Home from './pages/Home/HomePage';
 
 function App() {
   return (
 <>
+<BrowserRouter>
 <Header/>
+<Routes>
+  <Route exact={true} path='/' element={<Home/>}/>
+</Routes>
+</BrowserRouter>
 </>
   );
 }

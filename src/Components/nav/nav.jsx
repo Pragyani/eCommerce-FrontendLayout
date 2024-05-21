6deb409bc8426f126c6d601e7daa1008063fb9cf
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-21 20:34:40 +0530

    adding: list in nav-content

--- src/Components/nav/nav.jsx
+++ src/Components/nav/nav.jsx
@@ -3,29 +3,64 @@ import Button from '@mui/material/Button';
 import "./nav.css";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import GridViewIcon from '@mui/icons-material/GridView';
- 
-const Nav =()=>{
-    return(
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+    return (
         <>
-        <div className="nav ">
-            <div className="container">
-                <div className="row-line">
-                    
-                    <div className="browser-block">
-                    <Button className="bgtext"><GridViewIcon className="grid-icon"/>Browse All Categories <KeyboardArrowDownIcon/></Button>
-                    </div>
+            <div className="nav ">
+                <div className="container">
+                    <div className="row-line">
 
-                    <div className="nav-content">
-                        <nav> </nav>
-                    </div>
+                        <div className="browser-block">
+                            <Button className="bgtext"><GridViewIcon className="grid-icon" />Browse All Categories <KeyboardArrowDownIcon /></Button>
+                        </div>
 
-                    <div className="contact-block">
-                    </div>
+                        <div className="nav-content">
+                            <nav>
+                                <ul className="nav-list-item">
+                                    <li className="list-items">
+                                        <Button><Link>Home</Link></Button>
+                                    </li>
 
+                                    <li className="list-items">
+                                        <Button><Link>Home</Link></Button>
+                                    </li>
+
+                                    <li className="list-items">
+                                        <Button><Link>Home</Link></Button>
+                                    </li>
+
+                                    <li className="list-items">
+                                        <Button><Link>Home</Link></Button>
+                                    </li>
+
+                                    <li className="list-items">
+                                        <Button><Link>Home</Link></Button>
+                                    </li>
+
+                                    <li className="list-items">
+                                        <Button><Link>Home</Link></Button>
+                                    </li>
+
+                                    <li className="list-items">
+                                        <Button><Link>Home</Link></Button>
+                                    </li>
+
+                                    <li className="list-items">
+                                        <Button><Link>Home</Link></Button>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+
+                        <div className="contact-block">
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
-        
+
         </>
     )
 }

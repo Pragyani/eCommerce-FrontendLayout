36cdddad1ad0a75122ceb889d2ec7215735a6867
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-23 18:53:28 +0530

    importing Slider and adding classname

---
+++ src/pages/Home/slider/slider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Slider from "react-slick";
+ import './slider.css';
+    
+
+ const HomeSlider =()=>{
+    return(
+        <section className="homSlider">
+            <div className="sliderContainer">
+
+            </div>
+        </section>
+    )
+ }
+
+ export default HomeSlider;

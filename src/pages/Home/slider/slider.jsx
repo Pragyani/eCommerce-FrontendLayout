d4e3e076c0ec7823b77c6d759102f47d061f5d07
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-24 15:32:36 +0530

    adding: images in the Slider

--- src/pages/Home/slider/slider.jsx
+++ src/pages/Home/slider/slider.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 import Slider from "react-slick";
- import './slider.css';
-    
+import './slider.css';
+import { Slid2, slider1 } from "../../../Components/imagesURL/Images";
 
- const HomeSlider =()=>{
-    return(
+
+const HomeSlider = () => {
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+    return (
         <section className="homSlider">
             <div className="sliderContainer">
+                <Slider {...settings}>
+                    <div className="item">
+                        <img src={slider1} className="item-img" />
+                    </div>
+
+                    <div className="item">
+                        <img src={Slid2} className="item-img" />
+                    </div>
+                </Slider>
 
             </div>
         </section>
     )
- }
+}
 
- export default HomeSlider;
+export default HomeSlider;

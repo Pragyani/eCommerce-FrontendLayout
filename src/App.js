7d86ebb78eaf2b6ab1249b404a5c53b7c3876fb6
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-24 13:51:45 +0530

    Adding: about page in route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/header/header';
 import { BrowserRouter , Routes , Route} from 'react-router-dom';
 import Home from './pages/Home/HomePage';
+import About from './pages/About/Aboutpage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 <Header/>
 <Routes>
   <Route exact={true} path='/' element={<Home/>}/>
+  <Route exact={true} path='/about' element={<About/>}/>
 </Routes>
 </BrowserRouter>
 </>

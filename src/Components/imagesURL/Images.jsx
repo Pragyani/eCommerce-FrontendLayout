29c4b45b324b6be97ab1b66efcc3deef255bea49
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-25 14:47:44 +0530

    Adding : silder's images

--- src/Components/imagesURL/Images.jsx
+++ src/Components/imagesURL/Images.jsx
@@ -11,3 +11,10 @@ export default logoImg;
  export const cartIcon= "https://raw.githubusercontent.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/8ec2e9f2872233b520ce62eaef27d4e96268d0de/src/assets/images/icon-cart.svg"
 
  export const userIcon = "https://raw.githubusercontent.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/8ec2e9f2872233b520ce62eaef27d4e96268d0de/src/assets/images/icon-user.svg"
+
+
+ // sliders banner...//
+
+ export const slider1 = "https://github.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/blob/main/src/assets/images/slider-1.png?raw=true"
+
+ export const Slid2 ='https://github.com/rinkuv37/Ecommerce-Website-in-React-Js-with-Payment-Gateway/blob/main/src/assets/images/slider-2.png?raw=true'

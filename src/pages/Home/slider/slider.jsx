ccd8b56904715e8e3e71d847068d1bcda9a04c94
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-25 12:21:25 +0530

    adding:info in slider img

--- src/pages/Home/slider/slider.jsx
+++ src/pages/Home/slider/slider.jsx
@@ -7,7 +7,7 @@ import { Slid2, slider1 } from "../../../Components/imagesURL/Images";
 const HomeSlider = () => {
 
     var settings = {
-        dots: false,
+        dots: true,
         arrows: true,
         infinite: true,
         speed: 500,
@@ -21,11 +21,18 @@ const HomeSlider = () => {
             <div className="sliderContainer">
                 <Slider {...settings} className="home_slider_main">
                     <div className="item">
-                        <img src={slider1} className="item-img" />
+                        <img src={Slid2} className="item-img" />
+
+                        <div className="item-info">
+                            <h2 className="mb-4">
+                                Don't miss amazing <br />
+                                grocery deals
+                            </h2>
+                        </div>
                     </div>
 
                     <div className="item">
-                        <img src={Slid2} className="item-img" />
+                        <img src={slider1} className="item-img" />
                     </div>
                 </Slider>
 

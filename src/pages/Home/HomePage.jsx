e6d8b99bf21572c19ec6f27109a1bc62f5f09d52
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-25 19:42:20 +0530

    importing: SemiSlider Componnent

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import HomeSlider from "./slider/slider";
+import SemiSlider from "../../Components/semiSlider/semiSlid";
+ 
 const Home =()=>{
     return(
         <>
        <HomeSlider/>
+       <SemiSlider/>
+       <br /><br /><br /><br /><br />
         </>
     )
 }

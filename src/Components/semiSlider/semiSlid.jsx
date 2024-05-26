4c95ac55af8da50466216a1042224e3168328903
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-26 22:45:48 +0530

    adding: images and info

--- src/Components/semiSlider/semiSlid.jsx
+++ src/Components/semiSlider/semiSlid.jsx
@@ -7,12 +7,12 @@ const SemiSlider = () => {
 
     var settings = {
         dots: true,
-        // arrows: true,
+        arrows: true,
         infinite: true,
-        speed: 500,
-        slidesToShow: 10,
+        speed: 200,
+        slidesToShow: 9,
         slidesToScroll: 1
-      
+
     };
     return (
         <>
@@ -21,45 +21,87 @@ const SemiSlider = () => {
                 <div className="semiContainer">
                     <h3 className="semi-heading">Featured Categories</h3>
                     <Slider {...settings} className="semi_slider_Main">
-                               <div className="itemes">
-                                
-                               </div>
 
-                               <div className="itemes">
-                                
-                                </div>
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
 
-                                <div className="itemes">
-                                
-                                </div>
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
 
-                                <div className="itemes">
-                                
-                                </div>
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
 
-                                <div className="itemes">
-                                
-                                </div>
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
 
-                                <div className="itemes">
-                                
-                                </div>
 
-                                <div className="itemes">
-                                
-                                </div>
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
 
-                                <div className="itemes">
-                                
-                                </div>
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
 
-                                <div className="itemes">
-                                
-                                </div>
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
 
-                                <div className="itemes">
-                                
-                                </div>
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
+
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
+
+                        <div className="itemes">
+                            <div className="info">
+                                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-12.png" alt="productImg" />
+                                <h5>Cake & Milk</h5>
+                                <p>26 items</p>
+                            </div>
+                        </div>
                     </Slider>
                 </div>
             </div>

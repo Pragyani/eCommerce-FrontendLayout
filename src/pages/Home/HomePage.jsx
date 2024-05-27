18521b0664f6c824468a0b5b3b2a28a496aadbd8
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-27 22:04:48 +0530

    importing Banner Component

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeSlider from "./slider/slider";
 import SemiSlider from "../../Components/semiSlider/semiSlid";
+import Banner from "../../Components/banner/Banner";
  
 const Home =()=>{
     return(
         <>
        <HomeSlider/>
        <SemiSlider/>
+       <Banner/>
        <br /><br /><br /><br /><br />
         </>
     )

fbff36da15ebaa2135a0b838d0c8ba779821fa54
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-27 22:26:50 +0530

    adding: banner elements and images

--- src/Components/banner/Banner.jsx
+++ src/Components/banner/Banner.jsx
@@ -1,16 +1,28 @@
 import React  from "react";
 import './Banner.css';
-import { BannerImg1 } from "../imagesURL/Images";
+import { BannerImg1, BannerImg2, BannerImg3 } from "../imagesURL/Images";
 
 const Banner =()=>{
     return (
         <>
         <div className="banner-section">
-            <div className="contaier-fluid">
+            <div className="Container">
                 <div className="row">
                     <div className="banner-col">
                         <div className="box">
                             <img src={BannerImg1} alt="bannerPlate" />
+                        </div>
+                    </div>
+
+                    <div className="banner-col">
+                        <div className="box">
+                            <img src={BannerImg2} alt="bannerPlate" />
+                        </div>
+                    </div>
+
+                    <div className="banner-col">
+                        <div className="box">
+                            <img src={BannerImg3} alt="bannerPlate" />
                         </div>
                     </div>
 

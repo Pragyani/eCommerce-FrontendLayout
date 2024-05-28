0bcd4e82059368f8780a76d53ccaf8ab0e641b43
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-28 23:32:17 +0530

    added: image and info in producthumb

--- src/Components/product/product.jsx
+++ src/Components/product/product.jsx
@@ -5,7 +5,14 @@ const Product =()=>{
     return(
         <>
         <div className="productThumb">
-            
+            <div className="imgWrapper">
+                <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-2.jpg" alt="ProductImages" className="p-img"/>
+            </div>
+
+            <div className="info">
+                <span className="based-brand">Snacks</span>
+                <h4 className="tittle">Seeds of Change Organic Quinoa, Brown, & Red Rice </h4>
+            </div>
         </div>
         </>
     )

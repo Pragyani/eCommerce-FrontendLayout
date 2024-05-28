ad1390a4d447530f49e08d1a93447c3c0f0196d8
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-28 15:25:25 +0530

    adding: section for popular product

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import HomeSlider from "./slider/slider";
 import SemiSlider from "../../Components/semiSlider/semiSlid";
 import Banner from "../../Components/banner/Banner";
- 
-const Home =()=>{
-    return(
+
+const Home = () => {
+    return (
         <>
-       <HomeSlider/>
-       <SemiSlider/>
-       <Banner/>
-       <br /><br /><br /><br /><br />
+            <HomeSlider />
+            <SemiSlider />
+            <Banner />
+
+          <section className="homeProduct">
+            <div className="home-Container">
+                <h2 className="home-h2">
+                    Popular Products
+                </h2>
+            </div>
+          </section>
+            <br /><br /><br /><br /><br />
         </>
     )
 }

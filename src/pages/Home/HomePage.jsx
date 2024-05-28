60fa7e288fed87cfb4b826818bd1fd96c0b3c693
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-28 17:44:14 +0530

    added: home product listes

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './HomePage.css';
 import HomeSlider from "./slider/slider";
 import SemiSlider from "../../Components/semiSlider/semiSlid";
 import Banner from "../../Components/banner/Banner";
@@ -10,13 +11,43 @@ const Home = () => {
             <SemiSlider />
             <Banner />
 
-          <section className="homeProduct">
-            <div className="home-Container">
-                <h2 className="home-h2">
-                    Popular Products
-                </h2>
-            </div>
-          </section>
+            <section className="homeProduct">
+                <div className="home-Container">
+                    <div className="list-items">
+                        <h2 className="home-h2">Popular Products</h2>
+
+                        <ul className="list-inline">
+                            <li className="product-list">
+                                <a className="List">All</a>
+                            </li>
+
+                            <li className="product-list">
+                                <a className="List">Milks and Daires</a>
+                            </li>
+
+                            <li className="product-list">
+                                <a className="List">Coffes & Tea</a>
+                            </li>
+
+                            <li className="product-list">
+                                <a className="List">Pet Foods</a>
+                            </li>
+
+                            <li className="product-list">
+                                <a className="List">Meats</a>
+                            </li>
+
+                            <li className="product-list">
+                                <a className="List">Vegetables</a>
+                            </li>
+
+                            <li className="product-list">
+                                <a className="List">Fruits</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
             <br /><br /><br /><br /><br />
         </>
     )

94d477e18de841dc2ad5fab5c2bcf489b64fff76
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-01 22:18:59 +0530

    importing: Sliderslick and Product

--- src/pages/Home/bestSellslide/sellSlider.jsx
+++ src/pages/Home/bestSellslide/sellSlider.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { BannerImg4 } from "../../../Components/imagesURL/Images";
+import Slider from "react-slick";
+import Product from "../../../Components/product/product";
 
 const Sellslider = () => {
+
+    var settings = {
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 4,
+        fade: true
+    };
     return (
         <>
             <div className="list-i">
@@ -24,10 +36,16 @@ const Sellslider = () => {
             <br /><br />
             <div className="row">
                 <div className="col-img">
-                    <img src={BannerImg4} alt="" />
-
+                    <img src={BannerImg4} alt="banerimg4" />
                 </div>
-            </div>
+
+                    <div className="sliderContainer">
+                        <Slider {...settings} className="home_slider_main">
+                      <Product className="proSection"/>
+                        </Slider>
+                    </div>
+                </div>
+            
         </>
     )
 }

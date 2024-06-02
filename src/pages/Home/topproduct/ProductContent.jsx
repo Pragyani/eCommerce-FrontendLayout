25607504c64094d7329db7c52de477f8f266fa30
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-02 17:43:24 +0530

    adding: image in productCintent

--- src/pages/Home/topproduct/ProductContent.jsx
+++ src/pages/Home/topproduct/ProductContent.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { img1 } from "../../../Components/imagesURL/Images";
 
 const ProductContent = (props) => {
     return (
         <>
             <div className="product_box">
                 <h3>{props.title}</h3>
+
+                <div className="product_contnt">
+                    <div className="images">
+                        <img src={img1} alt="topProductIMAGES" />
+                    </div>
+
+                    <div className="images-info">
+
+                    </div>
+                </div>
+
             </div>
+
         </>
     )
 }

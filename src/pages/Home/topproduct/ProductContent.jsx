49326e0e8327d440126453bc62d38d12b3bd8bc4
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-02 21:53:49 +0530

    added: new image in ProductContent

--- src/pages/Home/topproduct/ProductContent.jsx
+++ src/pages/Home/topproduct/ProductContent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { img1 } from "../../../Components/imagesURL/Images";
+import { img1, img2, img3 } from "../../../Components/imagesURL/Images";
 import { Link } from "react-router-dom";
 import Rating from '@mui/material/Rating';
 
@@ -12,6 +12,38 @@ const ProductContent = (props) => {
                 <div className="product_contnt">
                     <div className="product-items">
                         <img src={img1} alt="topProductIMAGES" />
+
+                        <div className="images-info">
+                            <Link to=''> <h4>Nestle Original Coffee-Mate Coffee Creamer</h4></Link>
+                            <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly />
+                            <div className="info-price-Tg">
+                                <div className="price-info">
+                                    <span className="prices">$32.8</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="product_contnt">
+                    <div className="product-items">
+                        <img src={img3} alt="topProductIMAGES" />
+
+                        <div className="images-info">
+                            <Link to=''> <h4>Nestle Original Coffee-Mate Coffee Creamer</h4></Link>
+                            <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly />
+                            <div className="info-price-Tg">
+                                <div className="price-info">
+                                    <span className="prices">$32.8</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="product_contnt">
+                    <div className="product-items">
+                        <img src={img2} alt="topProductIMAGES" />
 
                         <div className="images-info">
                             <Link to=''> <h4>Nestle Original Coffee-Mate Coffee Creamer</h4></Link>

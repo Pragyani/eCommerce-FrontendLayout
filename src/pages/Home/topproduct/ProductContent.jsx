267d47bf09f096b71ae72f51e6baaf9eb19e9498
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-02 17:01:38 +0530

    aded: ProductContent

---
+++ src/pages/Home/topproduct/ProductContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductContent = (props) => {
+    return (
+        <>
+            <div className="product_box">
+                <h3>{props.title}</h3>
+            </div>
+        </>
+    )
+}
+export default ProductContent;

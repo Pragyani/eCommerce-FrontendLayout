fb36fed97ceecc6eab446564a3eee1298dd4321a
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-02 19:02:25 +0530

    adding: images-info

--- src/pages/Home/topproduct/ProductContent.jsx
+++ src/pages/Home/topproduct/ProductContent.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { img1 } from "../../../Components/imagesURL/Images";
+import { Link } from "react-router-dom";
+import Rating from '@mui/material/Rating';
 
 const ProductContent = (props) => {
     return (
@@ -8,13 +10,22 @@ const ProductContent = (props) => {
                 <h3>{props.title}</h3>
 
                 <div className="product_contnt">
-                    <div className="images">
+                    <div className="product-items">
                         <img src={img1} alt="topProductIMAGES" />
+
+                        <div className="images-info">
+                            <Link to=''> <h4>Nestle Original Coffee-Mate Coffee Creamer</h4></Link>
+                            <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly />
+                            <div className="info-price-Tg">
+                                <div className="price-info">
+                                    <span className="prices">$32.8</span>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
-                    <div className="images-info">
 
-                    </div>
                 </div>
 
             </div>

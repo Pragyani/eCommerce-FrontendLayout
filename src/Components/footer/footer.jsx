26dca7c9257d7c46263cb8bca9b56f7ec2a45401
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-03 22:16:14 +0530

    add: footer.jsx

---
+++ src/Components/footer/footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <>
+            <div className="footerWrapper">
+                <div className="footerBoxes">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col">
+
+                            </div>
+
+                            <div className="col">
+
+                            </div>
+
+                            <div className="col">
+
+                            </div>
+
+                            <div className="col">
+
+                            </div>
+
+                            <div className="col">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer;

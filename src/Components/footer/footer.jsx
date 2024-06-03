bc46063b8b4eb22b0732159254673ecc2440cee8
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-04 02:00:31 +0530

    Optimize the code

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -1,67 +1,14 @@
 import React from "react";
 import './footer.css';
-import { footerIcon1, footerIcon2, footerIcon3, footerIcon4, footerIcon5 } from "../imagesURL/Images";
+import FooterWrapper from "./footerWapper/FooterWrapper";
 
 const Footer = () => {
     return (
         <>
             <div className="footerWrapper">
-                    <div className="footer-container">
-                        <div className="fotter-row">
-                            <div className="col">
-                                 <div className="box">
-                                    <span><img src={footerIcon1} /></span>
-                                    <div className="info">
-                                        <h4>Best Price & offers</h4>
-                                        <p>Order $50 or more</p>
-                                    </div>
-                                 </div>
-                            </div>
-
-                            <div className="col">
-                            <div className="box">
-                                    <span><img src={footerIcon2}/></span>
-                                    <div className="info">
-                                        <h4>Free delivery</h4>
-                                        <p>24/7 amazing services</p>
-                                    </div>
-                                 </div>
-                            </div>
-
-                            <div className="col">
-                            <div className="box">
-                                    <span><img src={footerIcon3}/></span>
-                                    <div className="info">
-                                        <h4>Great daily deal</h4>
-                                        <p>When you sign up</p>
-                                    </div>
-                                 </div>
-                            </div>
-
-                            <div className="col">
-                            <div className="box">
-                                    <span><img src={footerIcon4}/></span>
-                                    <div className="info">
-                                        <h4>Wide assortment</h4>
-                                        <p>Mega Discounts</p>
-                                    </div>
-                                 </div>
-                            </div>
-
-                            <div className="col">
-                            <div className="box">
-                                    <span><img src={footerIcon5}/></span>
-                                    <div className="info">
-                                        <h4> Easy returns</h4>
-                                        <p>Within 30 days</p>
-                                    </div>
-                                 </div>
-                            </div>
-                        </div>
-                    </div>
+                   <FooterWrapper/>
             </div>
         </>
     )
 }
-
 export default Footer;

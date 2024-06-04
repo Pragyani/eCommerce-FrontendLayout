f221dc355d3e6d62584792103d33459837d3a57f
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-04 21:44:50 +0530

    add: footer bar in footer.jsx

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import './footer.css';
 import FooterWrapper from "./footerWapper/FooterWrapper";
+import { Link } from "react-router-dom";
+import logoImg from "../imagesURL/Images";
 
 const Footer = () => {
     return (
         <>
             <div className="footerWrapper">
                    <FooterWrapper/>
+
+                   <footer>
+                    <div className="footer-fluid">
+                        <div className="footer-row">
+                            <div className="footer-img">
+                               <Link> <img src={logoImg} alt="" /></Link>
+                               <p>Awesome grocery store website template</p>
+                            </div>
+                        </div> 
+                    </div>
+                   </footer>
             </div>
         </>
     )

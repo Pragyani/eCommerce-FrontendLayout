4e84859eb5cdae6ae728f002713ffef6472dc297
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-05 13:06:42 +0530

    add: material ui icon in footer element

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -3,6 +3,10 @@ import './footer.css';
 import FooterWrapper from "./footerWapper/FooterWrapper";
 import { Link } from "react-router-dom";
 import logoImg from "../imagesURL/Images";
+import PlaceIcon from '@mui/icons-material/Place';
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import ScheduleSendIcon from '@mui/icons-material/ScheduleSend';
+import AlarmOnIcon from '@mui/icons-material/AlarmOn';
 
 const Footer = () => {
     return (
@@ -16,6 +20,11 @@ const Footer = () => {
                             <div className="footer-img">
                                <Link> <img src={logoImg} alt="" /></Link>
                                <p>Awesome grocery store website template</p>
+                               <br />
+                               <p><PlaceIcon className="footer-uicon" /><span>Address:</span> 5171 W Campbell Ave <br  /> undefined Kent, Utah 53127 United States</p>
+                               <p><PhoneInTalkIcon className="footer-uicon" /><span>Call Us:</span>(+91) - 540-025-124553</p>
+                               <p><ScheduleSendIcon className="footer-uicon" /><span>Email:</span>sale@Nest.com</p>
+                               <p><AlarmOnIcon className="footer-uicon" /><span>Hours:</span>10:00 - 18:00, Mon - Sat</p>
                             </div>
                         </div> 
                     </div>

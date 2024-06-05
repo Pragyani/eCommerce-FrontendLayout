538fc07dcdbae8b7386bd29c5875a4edbf986323
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-05 13:39:20 +0530

    add: footer-wid-list

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -26,6 +26,21 @@ const Footer = () => {
                                <p><ScheduleSendIcon className="footer-uicon" /><span>Email:</span>sale@Nest.com</p>
                                <p><AlarmOnIcon className="footer-uicon" /><span>Hours:</span>10:00 - 18:00, Mon - Sat</p>
                             </div>
+
+                            <div className="wid-col">
+                                <h3>Company</h3>
+                                <ul className="foter-wid-list"></ul>
+                            <li><Link to="#">About US</Link></li>
+                            <li><Link to="#">Delicery Info.</Link></li>
+                            <li><Link to="#">Privacy Policy</Link></li>
+                            <li><Link to="#">Terms & Conditions</Link></li>
+                            <li><Link to="#">Contact Us</Link></li>
+                            <li><Link to="#">Supoort Center</Link></li>
+                            <li><Link to="#">Careers</Link></li>
+                            
+                            </div>
+
+
                         </div> 
                     </div>
                    </footer>

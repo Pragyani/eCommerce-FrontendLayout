5bbc59f079f9ecbcc7679fec9e93bc5eccb02c98
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-14 15:02:37 +0530

    adding : pinterest and youtube link and icon

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './footer.css';
 import FooterWrapper from "./footerWapper/FooterWrapper";
 import FooterSection from "./foteerSection/FooterSection";
-import { DailCol, IgIcon, TwitterIcon, fbicon } from "../imagesURL/Images";
+import { DailCol, IgIcon, TwitterIcon, fbicon, pinterestIcon, youTubeIcon } from "../imagesURL/Images";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -48,6 +48,8 @@ const Footer = () => {
                                 <Link to="#"><img src={fbicon} alt="facebook-icon" /></Link>
                                 <Link to="#"><img src={TwitterIcon} alt="facebook-icon" /></Link>
                                 <Link to="#"><img src={IgIcon} alt="facebook-icon" /></Link>
+                                <Link to="#"><img src={pinterestIcon} alt="facebook-icon" /></Link>
+                                <Link to="#"><img src={youTubeIcon} alt="facebook-icon" /></Link>
                             </div>
                         </div>
                     </div>

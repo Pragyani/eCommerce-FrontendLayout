99abeb541e70d3c960392a974c60265573e36b85
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-14 14:15:13 +0530

    add : dailing detail in footer

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './footer.css';
 import FooterWrapper from "./footerWapper/FooterWrapper";
 import FooterSection from "./foteerSection/FooterSection";
+import { DailCol } from "../imagesURL/Images";
 
 const Footer = () => {
     return (
@@ -15,12 +16,29 @@ const Footer = () => {
                     </div>
                 </footer>
 
-                <hr/>
+                <hr />
                 <div className="footer-bottom">
                     <div className="f-bottom">
                         <div className="footer-cr">
                             <p>© 2022, <b>Nest</b>- HTML Ecommerce Template <br />All rights reserved </p>
+                        </div>
 
+                        <div className="footer-col-dail">
+                            <div className="col-i-dail">
+                                <img src={DailCol} alt="dail-line" />
+                                <p>1900-2386
+                                    <span>Working 8:00 - 2:00</span>
+                                </p>
+                            </div>
+
+                            <div className="col-ii-dail">
+                                <div className="col-i-dail">
+                                    <img src={DailCol} alt="dail-line" />
+                                    <p>1900-2386
+                                        <span>24/7 Support Center</span>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>

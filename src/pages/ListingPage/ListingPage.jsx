c25516ec520387d276a1a42bdd217241afee73b4
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-16 20:54:07 +0530

    add: Listing Page

---
+++ src/pages/ListingPage/ListingPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './listPage.css';
+
+const ListingPage =()=>{
+    return(
+        <>
+          <section className="listingPAge">
+            <div className="container-fluid">
+               <div className="listing-header">
+            
+               </div>
+            </div>
+            </section>      
+        </>
+    )
+}
+
+export default ListingPage;

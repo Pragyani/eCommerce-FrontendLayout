012e8214040be24d3b603a81f3f619288cce9b7d
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-16 20:21:20 +0530

    add : footer in App.js file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/header/header';
-import { BrowserRouter , Routes , Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/HomePage';
 import About from './pages/About/Aboutpage';
+import ListingPage from './pages/ListingPage/ListingPage';
+import Footer from './Components/footer/footer';
 
 function App() {
   return (
-<>
-<BrowserRouter>
-<Header/>
-<Routes>
-  <Route exact={true} path='/' element={<Home/>}/>
-  <Route exact={true} path='/about' element={<About/>}/>
-</Routes>
-</BrowserRouter>
-</>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact={true} path='/' element={<Home />} />
+          <Route exact={true} path='/listingPage' element={<ListingPage />} />
+          <Route exact={true} path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 

7e031a499afeffe0b80e1e5f919d8f2c26fecaad
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-16 23:58:17 +0530

    add: headerSection of listPage

--- src/pages/ListingPage/ListingPage.jsx
+++ src/pages/ListingPage/ListingPage.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import './listPage.css';
+import { Link } from "react-router-dom";
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
-const ListingPage =()=>{
-    return(
+const ListingPage = () => {
+    return (
         <>
-          <section className="listingPAge">
-            <div className="container-fluid">
-               <div className="listing-header">
-            
-               </div>
-            </div>
-            </section>      
+            <section className="listingPAge">
+                <div className="container-fluid">
+                    <div className="listing-header">
+                        <h1>Snack</h1>
+                        <ul className="list-inline">
+                            <li className="list-item">
+                                <Link to={'/'} className="home-fnt"> <HomeOutlinedIcon className="arro"/>Home <KeyboardArrowRightIcon className="arrow"/></Link>
+                            </li>
+                            <li >
+                                <Link to={''}>Shop <KeyboardArrowRightIcon className="arrow"/></Link>
+                            </li>
+                            <li >
+                                <Link to={''}>Snack <KeyboardArrowRightIcon className="arrow"/></Link>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </section>
         </>
     )
 }

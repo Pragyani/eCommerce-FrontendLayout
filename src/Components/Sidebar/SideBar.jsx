e2f288eb036be5d60f8c18f2e8dda0ba7dac71e0
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-17 17:23:15 +0530

    add: SideBar.jsx

---
+++ src/Components/Sidebar/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SideBar = () => {
+    return (
+        <>
+            <div className="slideBar">
+                <div className="card">
+                    <h3>Category</h3>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SideBar;

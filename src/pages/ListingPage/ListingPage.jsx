0b94a94fd0bde41259a9a21c5213a251a0b417f3
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-17 16:49:10 +0530

    importing : SideBar Component

--- src/pages/ListingPage/ListingPage.jsx
+++ src/pages/ListingPage/ListingPage.jsx
@@ -3,6 +3,7 @@ import './listPage.css';
 import { Link } from "react-router-dom";
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import SideBar from "../../Components/Sidebar/SideBar";
 
 const ListingPage = () => {
     return (
@@ -13,17 +14,32 @@ const ListingPage = () => {
                         <h1>Snack</h1>
                         <ul className="list-inline">
                             <li className="list-item">
-                                <Link to={'/'} className="home-fnt"> <HomeOutlinedIcon className="arro"/>Home <KeyboardArrowRightIcon className="arrow"/></Link>
+                                <Link to={'/'} className="home-fnt"> <HomeOutlinedIcon className="arro" />Home <KeyboardArrowRightIcon className="arrow" /></Link>
                             </li>
                             <li >
-                                <Link to={''}>Shop <KeyboardArrowRightIcon className="arrow"/></Link>
+                                <Link to={''}>Shop <KeyboardArrowRightIcon className="arrow" /></Link>
                             </li>
                             <li >
-                                <Link to={''}>Snack <KeyboardArrowRightIcon className="arrow"/></Link>
+                                <Link to={''}>Snack <KeyboardArrowRightIcon className="arrow" /></Link>
                             </li>
                         </ul>
 
                     </div>
+
+                    <div className="listing-data">
+                        <div className="data-row">
+                            <div className="datarow-col">
+                                <SideBar />
+                            </div>
+
+                            <div className="row-col">
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
                 </div>
             </section>
         </>

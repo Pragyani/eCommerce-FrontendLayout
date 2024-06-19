b5fd1885aab39d6cafcce4e983f7c2d6443b9343
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-19 18:32:44 +0530

    Optimization : In SideBar.jsx

--- src/Components/Sidebar/SideBar.jsx
+++ src/Components/Sidebar/SideBar.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { CardImg } from "../imagesURL/Images";
 import Slider from '@mui/material/Slider';
-import Checkbox from '@mui/material/Checkbox';
+import CardData from "./CardData";
+import ItemSelection from "./itemSelection";
 
 
-function valuetext(value) {
-    return `${value}°C`;
-}
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+function valuetext(value) {return `${value}°C`};
 
 const SideBar = () => {
 
@@ -21,43 +18,7 @@ const SideBar = () => {
             <div className="slideBar">
                 <div className="card">
                     <h3>Category</h3>
-
-                    <div className="card-data">
-                        <div className="card-item">
-                            <span className="card-img">
-                                <img src={CardImg} alt="cardicoimage" width={30} />  </span>
-                            <h4 className="card-h4">Milks and Dairies</h4>
-                            <span className="card-quan">30</span>
-                        </div>
-
-                        <div className="card-item">
-                            <span className="card-img">
-                                <img src={CardImg} alt="cardicoimage" width={30} />  </span>
-                            <h4 className="card-h4">Clothing</h4>
-                            <span className="card-quan">32</span>
-                        </div>
-
-                        <div className="card-item">
-                            <span className="card-img">
-                                <img src={CardImg} alt="cardicoimage" width={30} />  </span>
-                            <h4 className="card-h4">Pet Foods</h4>
-                            <span className="card-quan">23</span>
-                        </div>
-
-                        <div className="card-item">
-                            <span className="card-img">
-                                <img src={CardImg} alt="cardicoimage" width={30} />  </span>
-                            <h4 className="card-h4">Baking Material</h4>
-                            <span className="card-quan">45</span>
-                        </div>
-
-                        <div className="card-item">
-                            <span className="card-img">
-                                <img src={CardImg} alt="cardicoimage" width={30} />  </span>
-                            <h4 className="card-h4">Fresh Fruit</h4>
-                            <span className="card-quan" >3</span>
-                        </div>
-                    </div>
+                    <CardData />
                 </div>
 
                 <div className="card">
@@ -71,27 +32,18 @@ const SideBar = () => {
                         onChange={handleChange}
                         valueLabelDisplay="auto"
                         getAriaValueText={valuetext}
-                        color="success"
-                    />
+                        color="success" />
 
                     <div className="priceRange">
                         <span>From: <strong className="txt">${[value[0]]}</strong></span>
                         <span>To: <strong className="txt">${[value[1]]}</strong></span>
                     </div>
 
-                    <div className="caption">
-                        <h5>Item Condition</h5>
+                    <ItemSelection />
 
-                        <ul>
-                            <li> <Checkbox {...label}  className="clr"/>New (1506)</li>
-                            <li> <Checkbox {...label} className="clr"/>Refurbised (27)</li>
-                            <li> <Checkbox {...label} className="clr"/>Used (45)</li>
-                        </ul>
-                    </div>
                 </div>
             </div>
         </>
     )
 }
-
 export default SideBar;

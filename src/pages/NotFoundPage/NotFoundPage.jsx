4940870aa9d2384d41d5f56f69d1120d65483a15
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-20 13:55:46 +0530

    add: backtoHome Button

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import './notfound.css';
 import { Page404 } from "../../Components/imagesURL/Images";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import KeyboardDoubleArrowRightOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowRightOutlined';
 
 const NotFound = () => {
     return (
@@ -11,7 +15,15 @@ const NotFound = () => {
                         <img src={Page404} alt="404Page" />
                         <br /><br />
                         <h1>Page Not Found</h1>
+                        <p>The link you clicked may be broken or the page may have been removed. <br />
+                            visit the<Link to='./'> Homepage </Link>or Contact us about the problem</p>
+
+                        <div className="backHome">
+                            <Link to='./'> <Button className="btn-home"><HomeOutlinedIcon className="i"/>Back To Home Page<KeyboardDoubleArrowRightOutlinedIcon className="arrow"/></Button></Link>
+                        </div>
                     </div>
+
+
                 </div>
             </section>
         </>

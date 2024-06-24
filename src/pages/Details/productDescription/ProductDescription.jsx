97b5ec5e8cb86c07eab4b658d87d783712645462
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-24 15:41:43 +0530

    Added : ProductDescription file

---
+++ src/pages/Details/productDescription/ProductDescription.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const ProductDescription = () => {
+    return (
+        <>
+            <div className="detailPageTab">
+                <div className="descriptionTab">
+                    <h3>Description</h3>
+                </div>
+
+                <div className="descriptnPara">
+                    <p> Uninhibited carnally hired played in whimpered dear gorilla koala depending and much yikes off far quetzal goodness and from for grimaced goodness unaccountably and <br /> meadowlark near unblushingly crucial scallop tightly neurotic hungrily some and dear furiously this apart.</p>
+                </div>
+
+                <div className="descriptnPara">
+                    <p>Spluttered narrowly yikes left moth in yikes bowed this that grizzly much hello on spoon-fed that alas rethought much decently richly and wow against the frequent fluidly at <br /> formidable acceptably flapped besides and much circa far over the bucolically hey precarious goldfinch mastodon goodness gnashed a jellyfish and one however because.</p>
+                </div>
+
+                <hr />
+
+                <div className="descriptionTab">
+                    <h3>Suggested Use</h3>
+                </div>
+                <div className="descriptnPara">
+                    <li>Refrigeration not necessary.</li>
+                </div>
+
+                <div className="descriptnPara">
+                    <li>Stir before serving.</li>
+                </div>
+
+                <hr />
+
+                <div className="descriptionTab">
+                    <h3>Warning</h3>
+                </div>
+                <div className="descriptnPara">
+                    <p>Oil separation occurs naturally. May contain pieces of shell.</p>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default ProductDescription;

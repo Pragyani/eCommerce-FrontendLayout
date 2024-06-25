eb966faa4c766b6a14b77c681bf5d160d5e1e3ca
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-25 12:45:37 +0530

    Added: useState in App componnet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Components/header/header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -12,6 +12,8 @@ import axios from 'axios';
 
 
 function App() {
+
+  const [productData, setProductData] = useState([]);
 
   useEffect(() => {
     getData()

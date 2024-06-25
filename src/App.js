59553bd35305ef8c4c44fc7f81546a61fe7cdfa8
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-25 12:44:39 +0530

    implement useEffect for API data fetching

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Components/header/header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -8,8 +8,24 @@ import ListingPage from './pages/ListingPage/ListingPage';
 import Footer from './Components/footer/footer';
 import NotFound from './pages/NotFoundPage/NotFoundPage';
 import DetailPage from './pages/Details/details';
+import axios from 'axios';
+
 
 function App() {
+
+  useEffect(() => {
+    getData()
+  }, []);
+
+  const getData = async(url)=>{
+    try{
+      const res = await axios('http://localhost:3000/productData');
+      console.log(res.data);
+
+    }catch(error){
+      console.log(error.message)
+    }
+  } 
   return (
     <>
       <BrowserRouter>
@@ -17,9 +33,9 @@ function App() {
         <Routes>
           <Route exact={true} path='/' element={<Home />} />
           <Route exact={true} path='/listingPage' element={<ListingPage />} />
-          <Route exact={true}path='/product/details' element={<DetailPage/>}/>
+          <Route exact={true} path='/product/details' element={<DetailPage />} />
           <Route exact={true} path='/about' element={<About />} />
-          <Route exact={true} path='*' element={<NotFound/>}/>
+          <Route exact={true} path='*' element={<NotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>

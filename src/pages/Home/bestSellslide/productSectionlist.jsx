965896a2030886acdacfed635229a3c475e0304b
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-06-26 20:38:17 +0530

    Added: dynamic data in li

--- src/pages/Home/bestSellslide/productSectionlist.jsx
+++ src/pages/Home/bestSellslide/productSectionlist.jsx
@@ -1,41 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const ProductSectionlist =()=>{
-    return(
+const ProductSectionlist = (props) => {
+    const { product } = props;
+
+    const [proData, setProData] = useState(product);
+    const [catArray, setCatArray] = useState([]);
+    // console.log(product, 'ani');
+
+    const catArr = []
+
+    useEffect(() => {
+        proData.length !== 0 &&
+            proData.map((item) => {
+                //  console.log(item);
+                item.items.length !== 0 &&
+                    item.items.map((item_) => {
+                        // console.log(item_)
+                        // console.log(item_.cat_name)
+                        catArr.push(item_.cat_name)
+                    })
+            })
+
+        setCatArray(catArr);
+        console.log(catArr, 'ani')
+    }, [])
+
+    return (
         <>
-          <div className="list-i">
-                        <h2 className="home-h2">Popular Products</h2>
+            <div className="list-i">
+                <h2 className="home-h2">Popular Products</h2>
 
-                        <ul className="list-inline">
-                            <li className="product-list">
-                                <a className="List">All</a>
-                            </li>
+                <ul className="list-inline">
 
-                            <li className="product-list">
-                                <a className="List">Milks and Daires</a>
-                            </li>
 
-                            <li className="product-list">
-                                <a className="List">Coffes & Tea</a>
-                            </li>
-
-                            <li className="product-list">
-                                <a className="List">Pet Foods</a>
-                            </li>
-
-                            <li className="product-list">
-                                <a className="List">Meats</a>
-                            </li>
-
-                            <li className="product-list">
-                                <a className="List">Vegetables</a>
-                            </li>
-
-                            <li className="product-list">
-                                <a className="List">Fruits</a>
-                            </li>
-                        </ul>
-                    </div>
+                    {
+                        catArray.map((cat, index) => {
+                            return (
+                                <li className="product-list">
+                                    <a className="List">{cat}</a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </>
     )
 }

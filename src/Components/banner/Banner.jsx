f637705f7ecb1214e6c91bc0083eac00317800d5
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-03 19:48:48 +0530

    add : Elements inside the image

--- src/Components/banner/Banner.jsx
+++ src/Components/banner/Banner.jsx
@@ -1,35 +1,46 @@
-import React  from "react";
+import React from "react";
 import './Banner.css';
 import { BannerImg1, BannerImg2, BannerImg3 } from "../imagesURL/Images";
+import { Button } from "@mui/material";
 
-const Banner =()=>{
+const Banner = () => {
     return (
         <>
-        <div className="banner-section">
-            <div className="Container">
-                <div className="row">
-                    <div className="banner-col">
-                        <div className="box">
-                            <img src={BannerImg1} alt="bannerPlate" />
+            <div className="banner-section">
+                <div className="Container">
+                    <div className="row">
+                        <div className="banner-col">
+                            <div className="box">
+                                <img src={BannerImg1} alt="bannerPlate" />
+                                <h4>Everyday Fresh & <br />
+                                    Clean with Our <br />
+                                    Products</h4>
+                                <span className="img-btn">Shop Now</span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="banner-col">
-                        <div className="box">
-                            <img src={BannerImg2} alt="bannerPlate" />
+                        <div className="banner-col">
+                            <div className="box">
+                                <img src={BannerImg2} alt="bannerPlate" />
+                                <h4>Make your Breakfast <br />
+                                    Healthy and Easy</h4>
+                                <span className="img-btn">Shop Now</span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="banner-col">
-                        <div className="box">
-                            <img src={BannerImg3} alt="bannerPlate" />
+                        <div className="banner-col">
+                            <div className="box">
+                                <img src={BannerImg3} alt="bannerPlate" />
+                                <h4>The best Organic <br />
+                                    Products Online</h4>
+                                <span className="img-btn">Shop Now</span>
+                            </div>
                         </div>
-                    </div>
 
-                    
+
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }

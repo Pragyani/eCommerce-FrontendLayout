832da3a8fd34ca51744af708e230f5cc4ad10f41
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-03 22:03:16 +0530

    Add : images in the about Page

--- src/pages/About/Aboutpage.jsx
+++ src/pages/About/Aboutpage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import './aboutPage.css';
+import { AboutPageImage, DishImage1, DishImage2, DishImage3 } from "../../Components/imagesURL/Images";
 
 const About = () => {
     return (
@@ -20,19 +21,26 @@ const About = () => {
                 <div className="container-block">
                     <div className="row">
                         <div className="col-lg">
-                            <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/page/about-1.png" alt="img" />
+                            <img src={AboutPageImage} alt="img" />
                         </div>
 
                         <div className="cntn">
                             <div className="content-form">
                                 <h2>Welcome to Nest</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate id est laborum.</p>
-                                <p>Ius ferri velit sanctus cu, sed at soleat accusata. Dictas prompta et Ut placerat legendos interpre.Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante Etiam sit amet orci eget. Quis commodo odio aenean sed adipiscing. Turpis massa tincidunt dui ut ornare lectus. Auctor elit sed vulputate mi sit amet. Commodo consequat. Duis aute irure dolor in reprehenderit in voluptate id est laborum.</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi consectetur inventore voluptate laborum commodi dicta numquam, architecto libero deleniti ea! sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate id est laborum.</p>
+                                <p>Ius ferri velit sanctus cu, sed at soleat accusata. Dictas prompta et Ut placerat  legendos interpre.Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante Etiam sit amet orci eget. Quis commodo odio aenean sed adipiscing. Turpis massa tincidunt dui ut ornare lectus. Auctor elit sed vulputate mi sit amet. Commodo consequat. Duis aute irure dolor in reprehenderit in voluptate id est laborum.</p>
 
                             </div>
+                            <div className="contnt-picture">
+                            <div className="rowes">
+                                <img src={DishImage1} alt="dishIamge" />
+                                <img src={DishImage2} alt="dishimg" />
+                                <img src={DishImage3} alt="dishimg" />
+                            </div>
+                        </div>
                         </div>
 
-                        
+                      
                     </div>
                 </div>
             </div>

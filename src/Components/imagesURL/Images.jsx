af95898185386d9127ef81fe47779b50093dcc1b
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-07-03 22:04:27 +0530

    Add : Images url

--- src/Components/imagesURL/Images.jsx
+++ src/Components/imagesURL/Images.jsx
@@ -2,97 +2,97 @@ const logoImg = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/logo.
 
 export default logoImg;
 
- export const compreIcon ="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-compare.svg"
+export const compreIcon = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-compare.svg"
 
- export const WhisListIcon= "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-heart.svg"
+export const WhisListIcon = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-heart.svg"
 
- export const cartIcon= "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-cart.svg"
+export const cartIcon = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-cart.svg"
 
- export const userIcon = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-user.svg"
-
-
- // sliders banner...//
-
- export const slider1 = "https://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-2.png"
-
- export const Slid2 ='https://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-1.png'
+export const userIcon = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-user.svg"
 
 
- //banner Images
+// sliders banner...//
 
- export const BannerImg1 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-1.png'
+export const slider1 = "https://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-2.png"
 
- export const BannerImg2 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-2.png';
-
- export const BannerImg3= 'https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-3.png';
-
- export const BannerImg4='https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-4.png'
+export const Slid2 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-1.png'
 
 
+//banner Images
 
- //topProductImges
+export const BannerImg1 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-1.png'
 
- export const img1 ='https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-1.jpg';
+export const BannerImg2 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-2.png';
 
- export const img2 ='https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-2.jpg';
+export const BannerImg3 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-3.png';
 
- export const img3 ='	https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-3.jpg';
+export const BannerImg4 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-4.png'
 
- 
- //LettersectionImage.
 
- export const LetterImg ='https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-9.png';
 
- //footer imges icon.
+//topProductImges
 
- export const footerIcon1 ='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-1.svg';
+export const img1 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-1.jpg';
 
- export const footerIcon2 ='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-2.svg';
+export const img2 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-2.jpg';
 
- export const footerIcon3 ='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-3.svg';
+export const img3 = '	https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-3.jpg';
 
- export const footerIcon4='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-4.svg';
 
- export const footerIcon5='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-5.svg';
+//LettersectionImage.
+
+export const LetterImg = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/banner/banner-9.png';
+
+//footer imges icon.
+
+export const footerIcon1 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-1.svg';
+
+export const footerIcon2 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-2.svg';
+
+export const footerIcon3 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-3.svg';
+
+export const footerIcon4 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-4.svg';
+
+export const footerIcon5 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-5.svg';
 
 
 //footer download app images.
 
 export const AppStoreimg = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/app-store.jpg';
 
-export const GooglePlayimg ='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/google-play.jpg'
+export const GooglePlayimg = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/google-play.jpg'
 
-export const PaymentGateway ='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/payment-method.png';
+export const PaymentGateway = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/payment-method.png';
 
 
 //footer dail image.
 
-export const DailCol ="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/phone-call.svg";
+export const DailCol = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/phone-call.svg";
 
 //footer social icon.
 
-export const fbicon ="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-facebook-white.svg";
- 
+export const fbicon = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-facebook-white.svg";
+
 export const TwitterIcon = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-twitter-white.svg";
 
 export const IgIcon = "	https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-instagram-white.svg";
 
-export const pinterestIcon ="https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-pinterest-white.svg";
+export const pinterestIcon = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-pinterest-white.svg";
 
 export const youTubeIcon = "https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-youtube-white.svg";
 
 
 // listing item images
 
-export  const CardImg='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-1.svg';
+export const CardImg = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-1.svg';
 
-export const CardImg2='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-2.svg';
+export const CardImg2 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-2.svg';
 
-export const CardImg3='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-3.svg';
+export const CardImg3 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-3.svg';
 
-export const CardImg4 ='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-4.svg';
+export const CardImg4 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-4.svg';
 
-export const CardImg5='https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-4.svg';
+export const CardImg5 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/category-4.svg';
 
 //sideBarImg
 
@@ -106,3 +106,13 @@ export const Page404 = 'https://wp.alithemes.com/html/nest/demo/assets/imgs/page
 //product image
 
 export const productdelImage = "https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-16-1.jpg"
+
+//aboutpage image
+
+export const AboutPageImage = "https://wp.alithemes.com/html/nest/demo/assets/imgs/page/about-1.png";
+
+export const DishImage1 = "https://wp.alithemes.com/html/nest/demo/assets/imgs/page/about-3.png"
+
+export const DishImage2 = "https://wp.alithemes.com/html/nest/demo/assets/imgs/page/about-4.png";
+
+export const DishImage3 = "https://wp.alithemes.com/html/nest/demo/assets/imgs/page/about-2.png";

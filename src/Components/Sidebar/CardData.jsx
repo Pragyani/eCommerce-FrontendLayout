bf1f90352cda3c6cb60c3066e37adae3fa5a4dce
PRAGYA PANDEY <pragyani2004@gmail.com>
2025-02-11 12:52:16 +0530

    add : align the code pf CardData.jsx

--- src/Components/Sidebar/CardData.jsx
+++ src/Components/Sidebar/CardData.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { CardImg, CardImg2, CardImg3, CardImg4, CardImg5 } from "../imagesURL/Images";
 import { Link } from "react-router-dom";
+import { CardImg } from "../imagesURL/Images";
 
 const CardData = (props) => {
     const { data } = props;
@@ -12,19 +12,20 @@ const CardData = (props) => {
                     data.length !== 0 &&
                     data.map((item, index) => {
                         return (
-                            <Link to={`/listingPage/${item.cat_name.toLowerCase()}`} className="link">
+                            <Link to={`/listingPage/${item.cat_name.toLowerCase()}`} className="link" key={index}>
                                 <div className="card-item">
                                     <span className="card-img">
-                                        <img src={CardImg} alt="cardicoimage" width={30} />  </span>
+                                        <img src={CardImg} alt="category image" width={30} />
+                                    </span>
                                     <h4 className="card-h4">{item.cat_name}</h4>
                                     <span className="card-quan">30</span>
                                 </div>
                             </Link>
-                        )
+                        );
                     })
                 }
             </div>
         </>
-    )
+    );
 }
 export default CardData;
